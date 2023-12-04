9c4da5e6a658b4791b448a00dc0f456271b9ebc5
Guillaume Carlino <35950782+guillaumecarlino@users.noreply.github.com>
2023-12-04 18:41:18 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
           {
             title: 'OpenAPI guide',
             path: '/guides/how-to/'
-          }
+          },
           {
             title: 'Events',
             path: '/guides/events/'
